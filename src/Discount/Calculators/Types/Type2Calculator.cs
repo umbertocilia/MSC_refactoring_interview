36fb629aff680138517a218c6ebe03eb841a2c0b
Umberto <umbertocilia@hotmail.it>
2022-03-14 09:48:18 +0100

    Projects and solutions division

---
+++ src/Discount/Calculators/Types/Type2Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorMSC
+{
+
+    public class Type2Calculator : AbrstractCalculator
+    {
+
+        public Type2Calculator() { }
+
+        public override decimal Calculate(int amount, int years)
+        {
+            checkInput(amount, years);
+            return (amount - (0.1m * amount)) - disc(amount, years) * (amount - (0.1m * amount));
+        }
+    }
+}
---
+++ src/Discount/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorMSC
+{
+
+    static public  class Customer
+    {
+        public enum Type
+        {
+            Unregistered = 1,
+            Registered = 2,
+            Valuable = 3,
+            MostValuable = 4
+        }
+    }
+
+}
